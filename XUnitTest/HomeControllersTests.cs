5766858986f6ecbc7045e3104cf306c24a55f00c
Nikita <grishinnikitaa@gmail.com>
2019-11-22 23:25:17 +0300

    Добавьте файлы проекта.

---
+++ XUnitTest/HomeControllersTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using sitedigitalstore.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTest
+{
+	public class HomeControllersTests
+	{
+		[Fact]
+		public void IndexViewDataMessage()
+		{
+			// Arrange
+			var loggerMoc = new Mock<ILogger<HomeController>>();
+
+			HomeController controller = new HomeController(loggerMoc.Object);
+
+			// Act
+			ViewResult result = controller.Index() as ViewResult;
+
+			// Assert
+			Assert.Equal("Hello!", result?.ViewData["Message"]);
+		}
+
+		[Fact]
+		public void IndexViewResultNotNull()
+		{
+			// Arrange
+			var loggerMoc = new Mock<ILogger<HomeController>>();
+
+			HomeController controller = new HomeController(loggerMoc.Object);
+			// Act
+			ViewResult result = controller.Index() as ViewResult;
+			// Assert
+			Assert.NotNull(result);
+		}
+	}
+}
+
---
+++ sitedigitalstore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace sitedigitalstore.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
